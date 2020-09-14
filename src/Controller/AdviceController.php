98986d96c3f310ecf297b5f8fa65465c9a838299
melineg14 <melinegeny14@gmail.com>
2020-09-14 18:37:31 +0200

    add article template

--- src/Controller/AdviceController.php
+++ src/Controller/AdviceController.php
@@ -8,11 +8,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdviceController extends AbstractController
 {
     /**
-     * @Route("/conseils", name="advices")
+     * @Route("/conseils", name="advices.index")
      * @return Response
      */
     public function index(): Response
     {
         return $this->render('pages/advice.html.twig');
+    }
+
+     /**
+     * @Route("/conseils/{id}", name="advices.article")
+     * @return Response
+     */
+    public function article(): Response
+    {
+        return $this->render('pages/article.html.twig');
     }
 }
