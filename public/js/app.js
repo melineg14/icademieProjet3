eeea475b23c3549c312cabfa927b99f4a0e5fe51
Gan Sabarat <gan.sabarat@gmail.com>
2020-09-15 23:43:20 +0200

    style and template modified

--- public/js/app.js
+++ public/js/app.js
@@ -63,3 +63,14 @@ jQuery('#datetimepicker').datetimepicker({
 $('#browse-file').fileselect({
   default: false // default: false, browser language
 });
+
+
+
+// Navbar add navbar-collapse-shadow class
+$('#main-nav-bar').find('.navbar:has(".in")').addClass('navbar-collapse-shadow');
+
+$('#main-nav-bar').on('shown.bs.collapse', function (e) {
+   $(e.target).closest('.navbar').addClass(' navbar-collapse-shadow');
+}).on('hidden.bs.collapse', function (e) {
+   $(e.target).closest('.navbar').removeClass(' navbar-collapse-shadow');
+})
