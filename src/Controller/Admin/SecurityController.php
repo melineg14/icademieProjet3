56d6611ab94b07cbc9552d1451548275f80499f9
melineg14 <melinegeny14@gmail.com>
2020-09-21 11:38:09 +0200

    finish authentification and security config

--- src/Controller/Admin/SecurityController.php
+++ src/Controller/Admin/SecurityController.php
@@ -1,18 +1,23 @@
-<?php 
+<?php
+
 namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/admin", name="connexion")
+     * @Route("/connexion", name="security.login")
      * @return Response
      */
-    public function index(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('admin/login.html.twig');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        return $this->render('admin/login.html.twig', [
+            'error' => $error
+        ]);
     }
 }
