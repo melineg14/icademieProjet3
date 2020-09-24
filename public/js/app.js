93f2d0b0f52214ade6ea7921d558362df20e597f
melineg14 <melinegeny14@gmail.com>
2020-09-24 11:24:34 +0200

    minify js and css

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,1 @@
-// Mobile navbar close ancre
-$('.ancre').on('click', function(){
-  $('.navbar-collapse').collapse('hide');
-});
-
-// Navbar add navbar-collapse-shadow class
-$('#main-nav-bar').find('.navbar:has(".in")').addClass('navbar-collapse-shadow');
-
-$('#main-nav-bar').on('shown.bs.collapse', function (e) {
-   $(e.target).closest('.navbar').addClass(' navbar-collapse-shadow');
-}).on('hidden.bs.collapse', function (e) {
-   $(e.target).closest('.navbar').removeClass(' navbar-collapse-shadow');
-})
+$(".ancre").on("click",(function(){$(".navbar-collapse").collapse("hide")})),$("#main-nav-bar").find('.navbar:has(".in")').addClass("navbar-collapse-shadow"),$("#main-nav-bar").on("shown.bs.collapse",(function(e){$(e.target).closest(".navbar").addClass(" navbar-collapse-shadow")})).on("hidden.bs.collapse",(function(e){$(e.target).closest(".navbar").removeClass(" navbar-collapse-shadow")}));
